075127e0506c5a0d446d44cb7af3b6efc3fbd38c
Anina Hort <aninahortmia@gmail.com>
2026-01-28 18:43:10 +0100

    adjusted the login

--- GlobeNotes/src/pages/Navigation.jsx
+++ GlobeNotes/src/pages/Navigation.jsx
@@ -1,37 +1,52 @@
 import { Link } from "react-router-dom";
-import { FaPlusCircle, FaGlobeEurope, FaMapMarkerAlt, FaHome, FaUser} from "react-icons/fa";
+import { FaPlusCircle, FaGlobeEurope, FaMapMarkerAlt, FaHome, FaUser } from "react-icons/fa";
+import { useContext } from "react";
+import { AuthContext } from "../auth/AuthContext";
 
 export default function Navigation() {
+  const { user, isAuthenticated } = useContext(AuthContext);
+
+  let loginClass = "navItem navLogin";
+
+  if (isAuthenticated) {
+    if (user?.role === "ADMIN") loginClass += " isAdmin";
+    else loginClass += " isUser"; // z.B. USER/PLAYER
+  }
+
   return (
     <nav className="main-nav">
       <ul>
-        <li>
+        <li className="navItem">
           <Link to="/">
             <FaHome size={44} style={{ marginRight: "8px" }} />
             Startseite
           </Link>
         </li>
-        <li>
+
+        <li className="navItem">
           <Link to="/hinzufuegen">
             <FaPlusCircle size={44} style={{ marginRight: "8px" }} />
             Hinzufügen
           </Link>
         </li>
-        <li>
+
+        <li className="navItem">
           <Link to="/weltkartepage">
             <FaGlobeEurope size={44} style={{ marginRight: "8px" }} />
             Weltkarte
           </Link>
         </li>
-        <li>
+
+        <li className="navItem">
           <Link to="/reiseziel">
             <FaMapMarkerAlt size={44} style={{ marginRight: "8px" }} />
             Reiseziel
           </Link>
         </li>
-        <li>
+
+        <li className={loginClass}>
           <Link to="/login">
-            <FaUser  size={44} style={{ marginRight: "8px" }} />
+            <FaUser size={44} style={{ marginRight: "8px" }} />
             Login
           </Link>
         </li>
